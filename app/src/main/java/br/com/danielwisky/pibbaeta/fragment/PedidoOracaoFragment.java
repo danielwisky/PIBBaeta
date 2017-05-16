c050ac282049f0c56e4580b8f9bd25d4fc5aa23d
Daniel Dias Wisky <daniel-ddw@hotmail.com>
2017-05-15 22:46:05 -0300

    versão inicial do projeto

---
+++ app/src/main/java/br/com/danielwisky/pibbaeta/fragment/PedidoOracaoFragment.java
@@ -0,0 +1,25 @@
+package br.com.danielwisky.pibbaeta.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.danielwisky.pibbaeta.R;
+
+public class PedidoOracaoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pedido_oracao, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle(R.string.pedido_oracao);
+    }
+}
---
+++ app/src/main/java/br/com/danielwisky/pibbaeta/fragment/ProgramacaoFragment.java
@@ -0,0 +1,25 @@
+package br.com.danielwisky.pibbaeta.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.danielwisky.pibbaeta.R;
+
+public class ProgramacaoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_programacao, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle(R.string.programacao);
+    }
+}
